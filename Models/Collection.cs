b787ba855031ea2f4ff860ed09b12950b44809a3
maxitect <maxime.downe@gmail.com>
2024-11-18 11:14:11 +0000

    chore: update Collection model to sit within namespace and enable database reference to Pokemon table

--- Models/Collection.cs
+++ Models/Collection.cs
@@ -1,7 +1,11 @@
-public class Collection : BaseEntity
+namespace PokeLikeAPI.Models
 {
-    public required string Name { get; set; }
-    public required string ThemeId { get; set; }
-    public int Likes { get; set; }
-    public required string PasswordHash { get; set; }
+    public class Collection : BaseEntity
+    {
+        public required string Name { get; set; }
+        public required string ThemeId { get; set; }
+        public int Likes { get; set; }
+        public required string PasswordHash { get; set; }
+        public ICollection<PokemonCollections> PokemonCollections { get; set; } = new List<PokemonCollections>();
+    }
 }
