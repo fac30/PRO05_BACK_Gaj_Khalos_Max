08d865ba0af83785c29dbd71fdfa18929cd40506
maxitect <maxime.downe@gmail.com>
2024-11-21 11:34:29 +0000

    fix: fix Theme model to have correct columns

--- Models/Theme.cs
+++ Models/Theme.cs
@@ -3,7 +3,5 @@ namespace PokeLikeAPI.Models
     public class Theme : BaseEntity
     {
         public required string Name { get; set; }
-        public required string ApiUrl { get; set; }
-        public int Likes { get; set; }
     }
 }
